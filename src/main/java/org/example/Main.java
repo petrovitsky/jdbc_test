55b4763d44999a6e2a4735d710ca56d4a2e29670
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2023-01-25 22:00:06 +0100

    init commot

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.h2.mvstore.db.Store;
+
+import java.nio.file.Files;
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        Storage storage = Storage.getInstance();
+
+    }
+}
